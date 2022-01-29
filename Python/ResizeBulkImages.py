5e441ce3c36ac92a6a8584eef45347d69ac2ec7e
Vishal Pal <72620289+vishalpal555@users.noreply.github.com>
2022-01-29 14:57:14 +0530

    Update and rename main.py to ResizeBulkImages.py

---
+++ Python/ResizeBulkImages.py
@@ -0,0 +1,38 @@
+import time
+from PIL import Image
+import os
+from concurrent.futures import ProcessPoolExecutor
+
+imagesPath = input("Enter the folder path: ")
+image_width = int(input("Enter new image width: "))
+image_height = int(input("Enter new image height: "))
+
+def resize(file):
+    try:
+        filePath = os.path.join(imagesPath, file)
+        img = Image.open(filePath)
+        #img = img.rotate(-90)  #if needs rotations
+        img = img.resize((image_width, image_height), Image.ANTIALIAS)
+        try:
+            new_imagesPath = os.path.join(imagesPath, f"ResizedImage_{image_width}x{image_height}")
+            img.save(f"{new_imagesPath}/{file}")
+        except FileNotFoundError:
+            print(f"Creating location: {new_imagesPath}")
+            os.mkdir(new_imagesPath)
+            img.save(f"{new_imagesPath}/{file}")
+            print(f"Saved {file} in {new_imagesPath}")
+        else:
+            print(f"Saved {file} in {new_imagesPath}")
+    
+    except Exception as e:
+        print(f"Problem resizing {file}")
+        print(e)
+
+files_list = os.listdir(imagesPath)
+
+start_time = time.time()
+with ProcessPoolExecutor() as executor:
+    result = executor.map(resize, files_list)
+end_time = time.time()
+
+print(f"Time taken: {end_time-start_time} s")
--- Python/main.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import numpy
-
-for i in range(68,141):
-    img = Image.open(f"with_mask/0 ({i}).jpg")
-    # img = img.rotate(-90)
-    img = img.resize((350,450), Image.ANTIALIAS)
-    img.save(f"with_mask_resized/0 ({i}).jpg")
